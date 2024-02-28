38dec5e5f5a1926340232ed5088cbd4671a922aa
Robert Fletcher <robert.fletcher@wlvfx.com>
2024-02-28 10:52:15 +0000

    fix path issues

--- build.py
+++ build.py
@@ -6,7 +6,6 @@ import typing
 import argparse
 
 
-
 def get_git_url(repo):
     return f"https://github.com/shotgunsoftware/{repo}"
 
@@ -16,11 +15,11 @@ def git_clone(url, directory):
     cmd = ["git", "clone", url, directory]
     subprocess.check_call(cmd)
 
+
 def stubgen(directory):
     print(f"Generating stubs for: {directory}")
     cmd = ["stubgen", directory]
     subprocess.check_call(cmd)
-
 
 
 def build(repo, source_rel, target_rels):
@@ -32,7 +31,7 @@ def build(repo, source_rel, target_rels):
     url = get_git_url(repo)
     if not os.path.exists(path):
         git_clone(url, path)
-    
+
     stubgen(os.path.join(path, source_rel))
 
     if not install:
@@ -40,11 +39,14 @@ def build(repo, source_rel, target_rels):
 
     for relpath in target_rels:
         source_path = os.path.join(cwd, relpath)
-        install_path = os.path.join(rez_install_path, os.path.basename(relpath))
+        install_path = os.path.join(
+            rez_install_path, "stubs", os.path.basename(relpath)
+        )
         print(f"Installing: {source_path} -> {install_path}")
         if os.path.exists(install_path):
             shutil.rmtree(install_path)
         shutil.copytree(source_path, install_path)
+
 
 def main():
     parser = argparse.ArgumentParser()
